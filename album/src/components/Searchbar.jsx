39f162b2b081f07aab1783c99f7e0249b00df109
mraduarte <mcduarte9@hotmail.com>
2025-03-26 21:13:29 -0300

    Incluindo css

--- album/src/components/Searchbar.jsx
+++ album/src/components/Searchbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Searchbar() {
+  const categorias = [
+    "Natureza",
+    "Pessoas",
+    "Tecnologia",
+    "Animais",
+    "Esportes",
+  ];
+
   return (
-    <div>Searchbar</div>
-  )
+    <div className="search-bar">
+      <input type="text" placeholder="Pesquisar fotos..." />
+      <button>Pesquisar</button>
+      <select>
+        {categorias.map((categoria) => (
+          <option value={categoria} key={categoria}>
+            {categoria}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
-export default Searchbar
+export default Searchbar;
